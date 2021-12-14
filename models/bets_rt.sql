5552bc0aa9fcc9ad101251d66a0e22d72f8eda3b
Anton Mantulo <interstorm.bi@gmail.com>
2021-12-14 13:33:21 +0000

    Fix

--- models/bets_rt.sql
+++ models/bets_rt.sql
@@ -30,7 +30,7 @@ WHERE (note LIKE 'ReturnAmountCausedByCompletion%'
   AND payitemname = 'UBS'
 
 {% if is_incremental() %}
-        and postingcompleted in ({{ partitions_to_replace | join(',') }})
+        and DATE(postingcompleted) >= CURRENT_DATE() -1
     {% endif %}
 
 )
@@ -181,5 +181,5 @@ FROM master
 WHERE postingcompleted is not null
 
 {% if is_incremental() %}
-        and postingcompleted in ({{ partitions_to_replace | join(',') }})
+        and DATE(postingcompleted) >= CURRENT_DATE() -1
     {% endif %}
